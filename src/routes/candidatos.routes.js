c81ceadf1792bd369dae5035bd0d070d542fd657
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2024-10-03 08:52:07 -0300

    feat: create index routes file

---
+++ src/routes/candidatos.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const routes = Router();
+
+routes.get("/", (req, res) => {
+    return res.status(200).send({ message: "Hello, world!"})
+
+});
+
+routes.get("/2tds2", (req, res) => {
+    return res.status(200).send({ message: "Hello, 2tds2!"})
+
+});
+
+routes.use("/emocoes", emocoesRoutes);
+
+export default routes;
